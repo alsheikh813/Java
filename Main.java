c769ca73bba564cc9d514e39953076a33964464b
firask89 <firask89@msn.com>
2019-01-12 15:45:02 +0200

    Create and instanitiate MyClass

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        MyClass ashraf = new MyClass();
+        ashraf.printMsg("hello firas");
+    }
+}
---
+++ MyClass.java
@@ -0,0 +1,5 @@
+public class MyClass {
+    public void printMsg(String msg){
+        System.out.println(msg);
+    }
+}
