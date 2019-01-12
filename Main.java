495e06bbcf100ee41ec896550b5093f84b4c542e
firask89 <firask89@msn.com>
2019-01-12 17:36:21 +0200

    (feat) add guessNumber method

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         MyClass ashraf = new MyClass();
         ashraf.printMsg("hello firas");
+        ashraf.guessMyNumber();
     }
 }
--- MyClass.java
+++ MyClass.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class MyClass {
-    public void printMsg(String msg){
+    public void printMsg(String msg) {
         System.out.println(msg);
+    }
+
+    public void guessMyNumber() {
+        Scanner input = new Scanner(System.in);
+        int rand =(int) Math.floor(Math.random() * 10);
+
+        for (int i =0; i < 3 ; i++){
+            System.out.println("please guess Number between 0 - 10 :");
+            int number = input.nextInt();
+            if(number == rand ){
+                System.out.println("Congratulation!! You win!!");
+                return;
+            }else{
+                System.out.println("Sorry !! Wrong guess Please try again :");
+
+            }
+        }
+        System.out.println("Sorry !! Game Over the number is :" + rand);
+
     }
 }
